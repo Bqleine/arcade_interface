aed7a8e14c29bc1485dd0caf8758f35d93e62fa1
maxencelupion <maxence.lupion@epitech.eu>
2024-03-21 12:32:11 +0100

    add: events

--- Events.hpp
+++ Events.hpp
@@ -12,8 +12,8 @@ namespace arcade {
         C, // ACTION IN GAME
         SPACE, // ACTION IN GAME
         Q, // QUIT PROGRAM
-        L, // CHANGE GRAPHIC
-        G, // CHANGE GAME
+        L, // NEXT GRAPHIC
+        G, // NEXT GAME
         R, // RESTART GAME
         ESCAPE, // GO TO MENU
         ENTER, // SELECTION
@@ -26,9 +26,13 @@ namespace arcade {
     };
 
     enum GameEventType {
-        LOAD_LIB,
-        LOAD_GAME,
+        NEXT_GAME,
+        NEXT_LIB,
+        LOAD_LIB, // USED FOR MENU ONLY
         QUIT_GAME, // SHOULD BE SEND BY THE GAME TO THE CORE TO CLOSE AND DELETE THE GAME
+        BACK_TO_MENU, // BACK TO MENU
+        EXIT, // EXIT PROGRAM
+        RESTART_GAME, // RESTART GAME
     };
 
     class GameEvent {
