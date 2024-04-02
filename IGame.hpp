adeab02a1e088e29063610e5a9f694d4c5c902d5
Theo Vermorel <theo.vermorel@epitech.eu>
2024-04-02 15:51:22 +0200

    [ADD] getter & setter for IGame

--- IGame.hpp
+++ IGame.hpp
@@ -15,5 +15,9 @@ namespace arcade::game {
         virtual std::vector<GameEvent> update(const std::vector<DisplayEvent> &events) = 0;
 
         virtual const std::vector<Sprite> &getSprites() const = 0;
+        
+        virtual const std::string &getUsername() const = 0;
+
+        virtual void setUsername(const std::string &username) = 0;
     };
 }
