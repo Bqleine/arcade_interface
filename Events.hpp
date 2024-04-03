923ed50ebc47d986be16bf019e9aa859fbe557bb
Theo Vermorel <theo.vermorel@epitech.eu>
2024-04-03 17:19:46 +0200

    [REMOVE] Button::OTHER useless when using std::vector<DisplayEvents> (in draw)

--- Events.hpp
+++ Events.hpp
@@ -11,7 +11,6 @@ namespace arcade {
         SPACE, // ACTION IN GAME
         ESCAPE, // GO TO MENU
         ENTER, // SELECTION
-        OTHER, // OTHER
         A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
     };
 
